ab59a7fd0661bb4e0f6b38903848acedab105283
Yorane Abbou <yorane.a@gmail.com>
2025-10-30 18:11:26 +0200

    Setting up Validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove properties unwanted
+      forbidNonWhitelisted: true, // If set to true, instead of stripping non-whitelisted properties validator will throw an erro
+      transform: true, // Convert automaticaly the given data type in the expected one
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((err) => console.error(err));
